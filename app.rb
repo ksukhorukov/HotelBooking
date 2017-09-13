414f068754e7ae30bd5de7ad6457e5d74e324da4
Kirill Sukhorukov <kirill.sukhorukov@overteam.ru>
2017-09-13 10:05:13 +0300

    Considered corner cases'

--- app.rb
+++ app.rb
@@ -1,18 +1,15 @@
 #!/usr/bin/env ruby
 
+require 'date'
+
 require 'pp'
 
 availabilities = [
-   { start_date: 1, end_date: 30, beds: 1 },
-   { start_date: 1, end_date: 45, beds: 2 },
-   { start_date: 10, end_date: 30, beds: 2 },
-   { start_date: 20, end_date: 45, beds: 3 },
-   { start_date: 1, end_date: 45, beds: 3 },
+   { start_date: (Date.parse '01.01.2017'), end_date: (Date.parse '30.01.2017'), beds: 1 },
 ]
 
 books = [
-  { start_date: 5, end_date: 15, beds: 1 },
-  { start_date: 20, end_date: 30, beds: 2 }
+  { start_date: (Date.parse '15.01.2017'), end_date: (Date.parse '29.01.2017'), beds: 1 },
 ]
 
 def get_rooms_with_certain_number_of_beds(availabilities, number_of_beds)
@@ -29,7 +26,13 @@ def check_if_available(room, start_date, end_date)
 end
 
 def reserve_room(availabilities, book, slot)
+
   availabilities.delete(slot)
+  availability_1 = {}
+  availability_2 = {}
+
+  return availabilities if slot[:start_date] == book[:start_date] && slot[:end_date] == book[:end_date]
+
   availability_1 = {
     start_date: slot[:start_date],
     end_date: book[:start_date],
@@ -40,9 +43,16 @@ def reserve_room(availabilities, book, slot)
     end_date: slot[:end_date],
     beds: slot[:beds]
   }
-  availabilities << availability_1
-  availabilities << availability_2
+
+  if slot[:end_date] == book[:end_date]
+    availabilities << availability_1
+  else
+    availabilities << availability_1
+    availabilities << availability_2
+  end
+
   availabilities
+
 end
 
 books.each do |book|
